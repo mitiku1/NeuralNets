b3a9cac664a449e2aa9d27296d5dc2fd6430d6e7
Mitiku <se.mitiku.yohannes@gmail.com>
2018-08-03 16:11:54 +0300

    added command files to generate dataset

---
+++ command.sh
@@ -0,0 +1,3 @@
+python  shapeset2_1cspo_2_3.10000.train.py write_formats amat
+python  shapeset2_1cspo_2_3.5000.test.py write_formats amat
+python  shapeset2_1cspo_2_3.5000.valid.py write_formats amat
